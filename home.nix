172cef5edb033dc84d1fa0bf36760b48af35f3bd
31nk <bruhmoment25077@gmail.com>
2025-02-11 15:21:03 -0500

    changed shell to fish

--- home.nix
+++ home.nix
@@ -1,22 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "dorian";
   home.homeDirectory = "/home/dorian";
+  home.stateVersion = "24.11";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
     pkgs.hyprland
     pkgs.neovim
@@ -24,10 +12,10 @@
     pkgs.swaynotificationcenter
     pkgs.foot
     pkgs.wofi
+    pkgs.fzf
+    pkgs.fish  # Add fish package
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".config/hypr".source = ~/dotfiles/hypr;
     ".config/nvim".source = ~/dotfiles/nvim;
@@ -37,29 +25,19 @@
     ".config/wofi".source = ~/dotfiles/wofi;
   };
 
-  # You can also set the file content immediately.
-  # ".gradle/gradle.properties".text = ''
-  #   org.gradle.console=verbose
-  #   org.gradle.daemon.idletimeout=3600000
-  # '';
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager.
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
+  home.sessionVariables = {};
   programs.home-manager.enable = true;
 
-  # Zsh and oh-my-zsh configuration
-  programs.zsh = {
+  # Fish shell configuration
+  programs.fish = {
     enable = true;
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "thefuck" ];
-    };
+    package = pkgs.fish;  # Explicitly enable the Fish package
+  };
+
+  home.shellAliases = {
+    rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config/#nixos";
+    upgrade="sudo nixos-rebuild switch --upgrade --flake ~/nixos-config/#nixos";
+    
   };
 }
 
