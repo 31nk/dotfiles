27b6fa703fa3584375aac988568e8459a43a4ecc
31nk <94924779+31nk@users.noreply.github.com>
2025-02-15 17:11:19 -0500

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "dorian";
+  home.username      = "dorian";
   home.homeDirectory = "/home/dorian";
-  home.stateVersion = "25.05";
+  home.stateVersion  = "25.05";
 
   home.packages = [
     pkgs.hyprland
@@ -13,39 +13,40 @@
     pkgs.foot
     pkgs.wofi
     pkgs.fzf
-    pkgs.fish      
-    pkgs.starship  
+    pkgs.fish
+    pkgs.starship
   ];
 
   home.file = {
-    ".config/hypr".source = ~/dotfiles/hypr;
-    ".config/nvim".source = ~/dotfiles/nvim;
-    ".config/waybar".source = ~/dotfiles/waybar;
-    ".config/swaync".source = ~/dotfiles/swaync;
-    ".config/foot".source = ~/dotfiles/foot;
-    ".config/wofi".source = ~/dotfiles/wofi;
+    ".config/hypr" = { source = ~/dotfiles/hypr; };
+    ".config/nvim" = { source = ~/dotfiles/nvim; };
+    ".config/waybar" = { source = ~/dotfiles/waybar; };
+    ".config/swaync" = { source = ~/dotfiles/swaync; };
+    ".config/foot" = { source = ~/dotfiles/foot; };
+    ".config/wofi" = { source = ~/dotfiles/wofi; };
 
-    ".config/fish/conf.d/starship.fish".text = ''
-      starship init fish | source
-    '';
+    ".config/fish/conf.d/starship.fish" = {
+      text = ''
+        starship init fish | source
+      '';
+    };
 
-    
-    ".config/starship.toml".source = ~/dotfiles/starship.toml;
+    ".config/starship.toml" = { source = ~/dotfiles/starship.toml; };
   };
 
   home.sessionVariables = {
     EDITOR = "nvim";
   };
+
   programs.home-manager.enable = true;
 
-  
   programs.fish = {
-    enable = true;
-    package = pkgs.fish;
-    shellInit = ''
+    enable       = true;
+    package      = pkgs.fish;
+    shellInit    = ''
       set -x EDITOR nvim
       set -x VISUAL nvim
-      '';
+    '';
     shellAliases = {
       rebuild    = "sudo nixos-rebuild switch --flake ~/nixos-config/#nixos";
       upgrade    = "sudo nixos-rebuild switch --upgrade --flake ~/nixos-config/#nixos";
